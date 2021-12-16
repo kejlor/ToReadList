52169e8311c0f2ebc13a2712606b5bf471e7f8ae
kejlor <woj450@gmail.com>
2021-12-16 20:32:36 +0100

    Initial Commit

---
+++ ToReadList/ToReadListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToReadListApp.swift
+//  ToReadList
+//
+//  Created by Bartosz Wojtkowiak on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToReadListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
